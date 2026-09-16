2b4e0bf7eedf9ec143d59c4e2aba23a21ea91b91
aaa <14751132@me.mcu.edu.tw>
2026-09-16 11:41:42 +0800

    Add files via upload

--- week02/week02-4.cpp
+++ week02/week02-4.cpp
@@ -1,1 +1,17 @@
-
+///week02-4.cpp Basic ²Ä2ÃD
+///LeetCode 389. Find the Difference
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int H[26] = {}
+        for (char c : s) {
+            H[c-'a'] += 1;
+        }
+        for (char c : t) {
+            H[c-'a'] -= 1;
+            if (H[c-'a']<0)
+                return c;
+        }
+        return 0;
+    }
+};
